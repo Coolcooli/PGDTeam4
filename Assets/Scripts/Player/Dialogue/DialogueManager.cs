c4bba13047175f1c2fea0f3af0acfe8f1ceda3e6
Wessel <rigterwessel@gmail.com>
2022-11-30 11:17:08 +0100

    adds pause

---
+++ Assets/Scripts/Player/Dialogue/DialogueManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    new private TMP_Text name;
+    private TMP_Text DialogueText;
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        StartCoroutine(RemoveDialogue());
+    }
+
+    public void EndDialogue()
+    {
+        //END DIALOGUE
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        DialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            DialogueText.text += letter;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator RemoveDialogue()
+    {
+        yield return new WaitForSeconds(5f);
+        DialogueText.text = "";
+    }
+}
