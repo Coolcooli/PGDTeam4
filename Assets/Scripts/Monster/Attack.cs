ab56c0718046aa34d80250fd6734dfd14b723270
Lars <lars.nienhuis@hva.nl>
2022-12-01 20:02:25 +0100

    Code clean

--- Assets/Scripts/Monster/Attack.cs
+++ Assets/Scripts/Monster/Attack.cs
@@ -1,46 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Attack : MonoBehaviour
 {
     [SerializeField]
-    float maxPlayerHeight = 100f;
-    [SerializeField]
     private Transform target;
-    public Transform Target { set { target = value; } }
+    [SerializeField]
+    private Transform startLocation;
 
     private WaypointSystem waypointSystem;
 
     private bool isAttacking = false;
-    private bool targetIsPlayer = false;
 
-    private void Start()
+    public UnityEvent hasKilledTarget;
+    private List<SkinnedMeshRenderer> skinnedMeshes = new List<SkinnedMeshRenderer>();
+
+    private void Awake()
     {
         waypointSystem = GetComponent<WaypointSystem>();
+        skinnedMeshes.AddRange(GetComponentsInChildren<SkinnedMeshRenderer>());
+        transform.position = startLocation.position;
     }
 
     private void Update()
     {
         CheckTargetDistance();
-        CheckTargetHeight();
     }
 
-    private void CheckTargetHeight()
-    {
-        if (target.transform.position.y >= maxPlayerHeight)
-        {
-            AttackTarget(target.transform);
-        }
-    }
-
-    private void AttackTarget(Transform target)
+    public void AttackTarget(Transform target)
     {
         waypointSystem.SetWaypoint(target);
         waypointSystem.MovementSpeed = 70f;
 
         isAttacking = true;
+    }
+
+    public void AttackCaptain()
+    {
+        print("attack");
+        foreach (SkinnedMeshRenderer mesh in skinnedMeshes)
+            mesh.enabled = true;
+        AttackTarget(target);
+        transform.LookAt(target);
+        hasKilledTarget.AddListener(DisableObject);
     }
 
     private void CheckTargetDistance()
@@ -49,14 +54,13 @@ public class Attack : MonoBehaviour
 
         if (Vector3.Distance(transform.position, waypointSystem.CurrentTarget.transform.position) < 1)
         {
-            if (targetIsPlayer)
-            {
-                SceneManager.LoadScene(1);
-            }
-            else
-            {
-                Destroy(target);
-            }
+            Destroy(target);
+            hasKilledTarget.Invoke();
         }
+    }
+
+    private void DisableObject()
+    {
+        Destroy(gameObject);
     }
 }
