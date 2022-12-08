e60cfba60106a53491c5517a48b2738afca8b130
Lars <lars.nienhuis@hva.nl>
2022-12-08 01:04:26 +0100

    save

--- Assets/Assetpacks/ScifiFacility/Scripts/Door_01.cs
+++ Assets/Assetpacks/ScifiFacility/Scripts/Door_01.cs
@@ -8,8 +8,8 @@ public class Door_01 : BaseDoor
     {
         base.OpenDoor();
 
-        wings[0]["door_01_wing_left"].speed = -1;
-        wings[1]["door_01_wing_right"].speed = -1;
+        wings[0]["door_01_wing_left"].speed = 1;
+        wings[1]["door_01_wing_right"].speed = 1;
         wings[0].Play();
         wings[1].Play();
     }
@@ -19,8 +19,8 @@ public class Door_01 : BaseDoor
 
         wings[0]["door_01_wing_left"].time = wings[0]["door_01_wing_left"].length;
         wings[1]["door_01_wing_right"].time = wings[1]["door_01_wing_right"].length;
-        wings[0]["door_01_wing_left"].speed = 1;
-        wings[1]["door_01_wing_right"].speed = 1;
+        wings[0]["door_01_wing_left"].speed = -1;
+        wings[1]["door_01_wing_right"].speed = -1;
         wings[0].Play();
         wings[1].Play();
     }
