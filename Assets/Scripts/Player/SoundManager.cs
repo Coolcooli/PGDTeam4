9b27f4f8d727309ac8b6402f7aa99ad4bf20bf52
Wessel <rigterwessel@gmail.com>
2022-12-22 17:06:20 +0100

    decouples the loop sound

--- Assets/Scripts/Player/SoundManager.cs
+++ Assets/Scripts/Player/SoundManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource[] sounds;
-
+    [SerializeField] private AudioSource[] loopSounds;
     private Dictionary<string, AudioSource> audioSources = new Dictionary<string, AudioSource>();
 
     private void Start(){
@@ -18,15 +19,76 @@ public class SoundManager : MonoBehaviour
     /// function that sets the items from the array in a dictionary on file name index
     /// </summary>
     private void StoreSounds(){
-        foreach (AudioSource sound in sounds){
-            Debug.Log(sound.name);
+        foreach (AudioSource sound in sounds)
             audioSources.Add(sound.name, sound);
-        }
+
+        foreach (AudioSource sound in loopSounds)
+            audioSources.Add(sound.name, sound);
     }
 
+    /// <summary>
+    /// plays a sound
+    /// </summary>
+    /// <param name="name">the name of the sound</param>
     public void Play(string name)
     {
-        audioSources[name].Play();
+        AudioSource audio = audioSources[name];
+
+        if(!CheckSound(audio, false)){
+            Debug.LogError(name + "is not the right sound type, use Startloop() instead");
+            return;
+        }
+
+        audio.Play();
+    }
+
+    /// <summary>
+    /// stops a sound
+    /// </summary>
+    /// <param name="name">name of the sound</param>
+    public void Stop(string name){
+        AudioSource audio = audioSources[name];
+
+        if(!CheckSound(audio, false)){
+            Debug.LogError(name + "is not the right sound type, use Stoploop() instead");
+            return;
+        }
+        audio.Stop();
+    }
+
+
+    /// <summary>
+    /// activates a loop sound
+    /// </summary>
+    /// <param name="name">name of the loopsound</param>
+    public void StartLoop(string name){
+
+        AudioSource audio = audioSources[name];
+        if(!CheckSound(audio, true)){
+            Debug.LogError(name + "is not the right sound type, use Start() instead");
+            return;
+        }
+        audio.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// deactivates a loop sound
+    /// </summary>
+    /// <param name="name">name of the loopsound</param>
+    public void StopLoop(string name){
+
+        AudioSource audio = audioSources[name];
+        if(!CheckSound(audio, true)){
+            Debug.LogError(name + "is not the right sound type, use Stop() instead");
+            return;
+        }
+        audio.gameObject.SetActive(false);
+
+    }
+    private bool CheckSound(AudioSource audio, bool isLoop){
+        if(isLoop)
+            return loopSounds.Contains(audio);
+        return sounds.Contains(audio);
     }
 
     /// <summary>
@@ -35,14 +97,17 @@ public class SoundManager : MonoBehaviour
     /// <param name="name">name of the audio source</param>
     /// <returns>whether the audio is playing or not, returns false and a warning when not found </returns>
     public bool isPlaying(string name){
-        if(audioSources[name] != null)
-        return audioSources[name].isPlaying;
+        AudioSource audio = audioSources[name];
+        if (audio == null)
+        {
+            Debug.LogWarning("audio " + name + " doesn't exist so it can't be playing");
+            return false;
+        }
 
-        Debug.LogWarning("audio " + name + " doesn't exist so it can't be playing");
-        return false;
+        if(sounds.Contains(audio))
+            return audioSources[name].isPlaying;
+
+        return audio.gameObject.activeSelf;
     }
 
-    public void Stop(string name){
-        audioSources[name].Stop();
-    }
 }
